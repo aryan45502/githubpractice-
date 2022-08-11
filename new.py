4fefccbaef9479aed437844755a0168b41b2fe8f
unknown <aryanghimire671@gmail.com>
2022-08-11 11:43:03 +0545

    String in python

---
+++ new.py
@@ -0,0 +1,12 @@
+# --> It represent a one line comment in python 
+
+'''
+It represent a multi-line comment 
+we can write any thing here
+'''
+
+a=5   #here, a is a variable which sores/holds integer type value i.e 5
+naam="Aryan"  #here, naam is a variable which stores/holds strig type value i.e "Aryan"
+
+# And in python string are always inside singe or double quotes:
+# for example :"Aryan" or 'Aryan'
