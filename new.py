fdbf5dbac6a859af65d0973a4bb7667502041b86
unknown <aryanghimire671@gmail.com>
2022-08-11 11:54:04 +0545

    String in python

--- new.py
+++ new.py
@@ -8,5 +8,7 @@ we can write any thing here
 a=5   #here, a is a variable which sores/holds integer type value i.e 5
 naam="Aryan"  #here, naam is a variable which stores/holds strig type value i.e "Aryan"
 
-# And in python string are always inside singe or double quotes:
+# And in python string are always inside single or double quotes:
 # for example :"Aryan" or 'Aryan'
+
+hello my name  
